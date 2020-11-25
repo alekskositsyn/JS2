7d00e2ee1a3007ba80d9b321ed7bb46eff8518ef
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2020-11-25 17:33:13 +0300

    end lesson4

--- project/js/main.js
+++ project/js/main.js
@@ -1,22 +1,5 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// Переведено на промисы
-// let getRequest = (url) => {
-//     return new Promise((resolve, reject) => {
-//         let xhr = new XMLHttpRequest();
-//         xhr.open("GET", url, true);
-//         xhr.onreadystatechange = () => {
-//             if(xhr.readyState === 4){
-//                 if(xhr.status !== 200){
-//                     reject('Error');
-//                 } else {
-//                     resolve(xhr.responseText);
-//                 }
-//             }
-//         };
-//         xhr.send();
-//     })
-// };
 
 /**
  * Описываем базовые классы
