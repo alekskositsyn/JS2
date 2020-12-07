c31e29d7be08286246b580426ea1b2f20f0adf18
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2020-12-07 13:04:40 +0300

    install nodejs

--- project/public/js/ProducComp.js
+++ project/public/js/ProducComp.js
@@ -1,22 +1,22 @@
 Vue.component('products', {
-    data() {
+    data(){
         return {
-            catalogUrl: '/catalogData.json',
+            catalogUrl: '',
             products: [],
             filtered: [],
             imgCatalog: 'https://placehold.it/200x150',
         }
     },
     methods: {
-        filter(value) {
+        filter(value){
             let regexp = new RegExp(value, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
-    mounted() {
-        this.$parent.getJson(`${API + this.catalogUrl}`)
+    mounted(){
+        this.$parent.getJson('/api/products')
             .then(data => {
-                for (let el of data) {
+                for(let el of data){
                     this.products.push(el);
                     this.filtered.push(el);
                 }
@@ -24,21 +24,21 @@ Vue.component('products', {
     },
     template: `
         <div class="products">
-            <product ref="product" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+            <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
         </div>
     `
 });
 Vue.component('product', {
     props: ['product', 'img'],
     data() {
-        return {
-            /**
-             * Создали ссылку на API нашей корзины. Т.к. все компоненты у нас регистрируются в корневом экземпляре Vue,
-             * то мы легко можем получить доступ к ним используя свойство $root.
-             * $parent можно использовать для доступа к родительскому экземпляру из дочернего.
-             */
-            cartAPI: this.$root.$refs.cart,
-        };
+      return {
+          /**
+           * Создали ссылку на API нашей корзины. Т.к. все компоненты у нас регистрируются в корневом экземпляре Vue,
+           * то мы легко можем получить доступ к ним используя свойство $root.
+           * $parent можно использовать для доступа к родительскому экземпляру из дочернего.
+           */
+          cartAPI: this.$root.$refs.cart,
+      };
     },
 
     template: `
